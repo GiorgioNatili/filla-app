840ba8b847cdfd4933f747e5e53d9069a9bbebd1
GiorgioNatili <g.natili@gnstudio.com>
2016-08-23 19:37:50 -0700

    Fix test environment

---
+++ Filla/Login/view/LoginInputValidationDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  ValidationDelegate.swift
+//  Filla
+//
+//  Created by Natili, Giorgio on 8/23/16.
+//  Copyright © 2016 Giorgio Natili. All rights reserved.
+//
+
+import UIKit
+import SwiftValidator
+
+class ValidationDelegate: NSObject, ValidationDelegate {
+
+    // MARK: - ValidationDelegate implementation
+    func validationSuccessful() {
+        
+    }
+    
+    func validationFailed(errors: [UITextField:ValidationError]) {
+        
+        
+    }
+}
